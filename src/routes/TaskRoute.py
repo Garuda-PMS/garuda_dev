3c9fbc02ce945b226a8a76c99f32a56135b982d2
Abhinav <abve9575@colorado.edu>
2022-04-10 01:43:01 -0600

    Delete src folder

--- src/routes/TaskRoute.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from controllers.TaskController import view, create, edit
-
-task_blueprint = Blueprint('task_blueprint', __name__)
-
-task_blueprint.route('/task/view/<int:task_id>', methods=['GET'])(view)
-task_blueprint.route('/task/create', methods=['POST'])(create)
-task_blueprint.route('/task/edit/<int:task_id>', methods=['PUT'])(edit)
