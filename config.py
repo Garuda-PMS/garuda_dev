bb567d42e1f76704e982fa8b28609caba0a3e434
Abhinav <abve9575@colorado.edu>
2022-04-10 23:35:39 -0600

    Update config with secret

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # TODO Load credentials from environment variables
+    SECRET_KEY = 'test'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/garuda_pms'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
